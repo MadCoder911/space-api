ce6767ac314cbf47ae0572c048e7f34a0f7f28fc
Royce119 <madcoder911@gmail.com>
2023-09-16 23:35:09 +0300

    added proper documentation for mongo connections file

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const MONGO_URL = process.env.MONGO_URL;
+//Connect to mongo DB using mongoose
 mongoose.connection.once("open", () => {
   console.log("Connection Ready");
 });
+//Log any errors in the DB connection
 mongoose.connection.on("error", (err) => {
   console.log(err);
 });
-
+//Connect to mongo
 async function mongoConnect() {
   await mongoose.connect(MONGO_URL);
 }
