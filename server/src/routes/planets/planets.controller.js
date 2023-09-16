62d9132ed5ecf037475eb23e7d232d2a584ab4cc
Royce119 <madcoder911@gmail.com>
2023-09-16 23:33:01 +0300

    added proper documentation for planets controller

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,4 +1,5 @@
 const { getAllPlanets } = require("../../models/planets.mode");
+//Get all planets from the database
 async function httpGetAllPlanets(req, res) {
   return res.status(200).json(await getAllPlanets());
 }
